e71c6da76f053d6124b946534c04adf911ed0de6
charlie <charlie.okonomi@gmail.com>
2021-03-09 02:51:55 -0500

    asdf

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
 args = parser.parse_args()
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cuda'
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
